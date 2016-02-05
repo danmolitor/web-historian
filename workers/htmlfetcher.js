bbfee52a28f9e406c008f0766289041bf452bd91
danmolitor <danmolitor91@gmail.com>
2016-02-04 20:23:46 -0800

    downloadUrls passed. Cron somewhat implimented

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,11 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var CronJob = require('cron').CronJob;
+var request = require('request');
+var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var path = require('path');
+
+archive.readListOfUrls(archive.downloadUrls());
+
+fs.appendFile(__dirname + '/log.txt', 'cron job executed at' + new Date() + '\n', function(err, written, string) {});
